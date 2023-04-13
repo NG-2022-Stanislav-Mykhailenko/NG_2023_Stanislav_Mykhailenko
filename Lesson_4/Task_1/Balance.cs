20d02a283a5da062726555cb8c8de17a6bf12693
Stanislav Mykhailenko <mykhailenkostanislavzno@gmail.com>
2023-04-13 17:54:09 +0300

    Update Lesson 4 Task 1

---
+++ Lesson_4/Task_1/Balance.cs
@@ -0,0 +1,32 @@
+﻿namespace SingleResponsibility
+{
+    public class Balance
+    {
+        private readonly CustomerList _customerList;
+
+        public Balance(CustomerList customerList)
+        {
+            _customerList = customerList;
+        }
+
+        public void GetBalance(int id)
+        {
+            Console.WriteLine($"Your balance is: {GetBalanceById(id)}");
+        }
+
+        public decimal? GetBalanceById(int id)
+        {
+            var customer = _customerList.GetById(id);
+            if (customer != null)
+                return customer.Balance;
+            return null;
+        }
+
+        public void UpdateBalance(int id, decimal newBalance)
+        {
+            var customer = _customerList.GetById(id);
+            if (customer != null)
+                customer.Balance = newBalance;
+        }
+    }
+}
--- Lesson_4/Task_1/CustomerList.cs
+++ Lesson_4/Task_1/CustomerList.cs
@@ -8,30 +8,9 @@
             new Customer(2, "VHarbar", 100000)
         };
 
-        public void GetBalance(int id)
-        {
-            Console.WriteLine($"Your balance is: {GetBalanceById(id)}");
-        }
-
-        public decimal? GetBalanceById(int id)
-        {
-            var customer = Customers.FirstOrDefault(x => x.Id == id);
-            if (customer != null)
-                return customer.Balance;
-            else
-                return null;
-        }
-
         public Customer? GetById(int id)
         {
             return Customers.FirstOrDefault(x => x.Id == id);
-        }
-
-        public void UpdateBalance(int id, decimal newBalance)
-        {
-            var customer = GetById(id);
-            if (customer != null)
-                customer.Balance = newBalance;
         }
     }
 }
