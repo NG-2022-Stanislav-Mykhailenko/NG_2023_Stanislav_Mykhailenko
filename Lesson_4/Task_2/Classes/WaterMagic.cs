505a63f5d43577e0a422b5791e4b44b5ca4c6f10
Stanislav Mykhailenko <mykhailenkostanislavzno@gmail.com>
2023-04-16 19:28:58 +0300

    Update Lesson 4 Task 2 to remove useless code

--- Lesson_4/Task_2/Classes/WaterMagic.cs
+++ Lesson_4/Task_2/Classes/WaterMagic.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Open_Closed.Interfaces;
 
 namespace Open_Closed.Classes
